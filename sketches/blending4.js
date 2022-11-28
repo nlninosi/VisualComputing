f8ee9f23d403c4fa517ec10d4b7438d52a66d7b8
sladinor1 <sladinor1@users.noreply.github.com>
2022-11-28 19:25:58 +0000

    deploy: be35c3242f287edfc19fa9c199820f04bd41728e

---
+++ sketches/blending4.js
@@ -0,0 +1,104 @@
+let colorShader;
+let vector;
+let vector2;
+let RCB;
+let GCB;
+let BCB;
+
+function preload() {
+  // The vertex shader defines how vertices are projected onto clip space.
+  // Most of the times a projection and modelview matrix are needed for it:
+  // https://visualcomputing.github.io/docs/shaders/programming_paradigm/
+  // Here, however, we are going to:
+  // 1. Define the triangle vertices directly in clip space, thus bypassing
+  // both of these matrices (matrices: Tree.NONE). The p5 mandelbrot vertex
+  // shader does just the same: https://p5js.org/reference/#/p5/loadShader
+  // 2. Interpolate vertex color data (varyings: Tree.color4). Note that
+  // color data is defined in a per vertex basis with the p5 fill command.
+  // Have a look at the generated vertex shader in the console!
+  // readShader: https://github.com/VisualComputing/p5.treegl#handling
+  colorShader = readShader('/showcase/sketches/shaders/blending2.frag',
+                          { matrices: Tree.NONE, varyings: Tree.NONE });
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(300, 300, WEBGL);
+  colorPicker = createColorPicker('#ed225d');
+  colorPicker2 = createColorPicker('#1AAf18');
+  colorPicker.position(0, 0);
+  colorPicker2.position(150, 0);
+  slider = createSlider(0, 1, 0, 0.01);
+  slider.position(10, 270);
+  RCB = createCheckbox('RED', false);
+  GCB = createCheckbox('GREEN', false);
+  BCB = createCheckbox('BLUE', false);
+  // https://p5js.org/reference/#/p5/shader
+  //shader(colorShader);
+}
+
+function draw() {
+  background(0);
+  // the fill command is used to define the colors
+  // (to be interpolated) in a per-vertex basis
+  beginShape();
+  fill("white");
+  vertex(-0.5, 0.5);
+  vertex(0, 0.5);
+  vertex(0, -0.5);
+  vertex(-0.5, -0.5);
+  endShape();
+  shader(colorShader);
+  beginShape();
+  vertex(0, 0.5);
+  vertex(0.5, 0.5);
+  vertex(0.5, -0.5);
+  vertex(0, -0.5);
+  endShape();
+  //console.log(colorPicker.color().levels[3])
+  let c1 = normalizar(colorPicker.color().levels,1);
+  let c2 = normalizar(colorPicker2.color().levels,1);
+  if (RCB.checked()){
+    c1[0]=slider.value();
+    c2[0]=slider.value();
+  }
+  if (GCB.checked()){
+    c1[1]=slider.value();
+    c2[1]=slider.value();
+  }
+  if (BCB.checked()){
+    console.log("b")
+    c1[2]=slider.value();
+    c2[2]=slider.value();
+  }
+  c1[2] = 1; 
+  c2[2] = 1; 
+  colorShader.setUniform('uMaterial1', c1);
+  colorShader.setUniform('uMaterial2', c2);
+}
+
+function keyPressed() {
+  if (key == 'c') {
+    //normalizar(colorPicker.color().levels,1)
+    let c1 = normalizar(colorPicker.color().levels,1);
+    let c2 = normalizar(colorPicker2.color().levels,1);
+    c1[2] = slider.value(); 
+    c2[2] = slider.value(); 
+    console.log(c1);
+    colorShader.setUniform('uMaterial1', c1);
+    colorShader.setUniform('uMaterial2', c2);
+  }
+}
+
+function normalizar(arr, max) {
+  // find the max value
+  var m = 0;
+  for(var x=0; x<arr.length; x++) {
+    m = Math.max(m, arr[x]);
+  }
+  // find the ratio
+  var r = max / m;
+  // normalize the array
+  for(var x=0; x<arr.length; x++) arr[x] = arr[x] * r;
+  return arr;
+}
--- sketches/coloring.js
+++ sketches/coloring.js
@@ -1,4 +1,6 @@
-let colorShader;
+/*let colorShader;
+let colorPicker;
+let colorPicker2;
 
 function preload() {
     // The vertex shader defines how vertices are projected onto clip space.
@@ -19,45 +21,25 @@ function preload() {
 function setup() {
     // shaders require WEBGL mode to work
     createCanvas(380, 380, WEBGL);
-    //background(0);
-    shader(colorShader);
+    colorPicker = createColorPicker('#ed225d');
+    colorPicker2 = createColorPicker('#1AAf18');
+    colorPicker.position(0, 0);
+    colorPicker2.position(150, 0);
+    //shader(colorShader);
     // https://p5js.org/reference/#/p5/shader
 
 }
 
-let v = 'rgba(255,165,71,0.65)'
-let v2 = 'rgba(238,130,238,0.65)'
-
 function draw() {
-    //shader(colorShader);
-    //rect(0,0,50, 50);
     background(0);
-    colorShader.setUniform('uMaterial1', '(255,165,71,0.75)');
-    colorShader.setUniform('uMaterial2', '(238,130,238,0.75)');
+    beginShape();
+    vertex(-50, -50);
+    vertex(50, -50);
+    vertex(50, 50);
+    vertex(-50, 50);
+    endShape();
     // the fill command is used to define the colors
     //(to be interpolated) in a per-vertex basis
-    beginShape();
-    fill(255,165,71);
-    vertex(-180, -180);
-    vertex(-30, -180);
-    vertex(-30, -30);
-    vertex(-180, -30);
-    endShape(CLOSE);
-    /*beginShape();
-    fill(238,130,238);
-    vertex(0, -180);
-    vertex(150, -180);
-    vertex(150, -30);
-    vertex(0, -30);
-    endShape(CLOSE);
-    beginShape();
-    fill("white")
-    //shader(colorShader);
-    vertex(-80, 0);
-    vertex(70, 0);
-    vertex(70, 150);
-    vertex(-80, 150);
-    endShape(CLOSE);*/
 }
 
 // vertices are given directly in clip-space,
@@ -65,6 +47,86 @@ function draw() {
 function keyPressed() {
   if (key == 'c') {
     // https://p5js.org/reference/#/p5.Shader/setUniform
-    colorShader.setUniform();
+    colorShader.setUniform('uMaterial1', colorPicker.color().levels);
+    colorShader.setUniform('uMaterial2', colorPicker2.color().levels);
   }
+}*/
+let colorShader;
+let vector;
+let vector2;
+
+function preload() {
+  // The vertex shader defines how vertices are projected onto clip space.
+  // Most of the times a projection and modelview matrix are needed for it:
+  // https://visualcomputing.github.io/docs/shaders/programming_paradigm/
+  // Here, however, we are going to:
+  // 1. Define the triangle vertices directly in clip space, thus bypassing
+  // both of these matrices (matrices: Tree.NONE). The p5 mandelbrot vertex
+  // shader does just the same: https://p5js.org/reference/#/p5/loadShader
+  // 2. Interpolate vertex color data (varyings: Tree.color4). Note that
+  // color data is defined in a per vertex basis with the p5 fill command.
+  // Have a look at the generated vertex shader in the console!
+  // readShader: https://github.com/VisualComputing/p5.treegl#handling
+  colorShader = readShader('/showcase/sketches/shaders/color.frag',
+                          { matrices: Tree.NONE, varyings: Tree.NONE });
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(300, 300, WEBGL);
+  colorPicker = createColorPicker('#ed225d');
+  colorPicker2 = createColorPicker('#1AAf18');
+  colorPicker.position(0, 0);
+  colorPicker2.position(150, 0);
+  slider = createSlider(0, 1, 0.5, 0.01);
+  slider.position(10, 270);
+  // https://p5js.org/reference/#/p5/shader
+  shader(colorShader);
+}
+
+function draw() {
+  background(0);
+  // the fill command is used to define the colors
+  // (to be interpolated) in a per-vertex basis
+  beginShape();
+  vertex(-0.5, 0.5);
+  vertex(0.5, 0.5);
+  vertex(0.5, -0.5);
+  vertex(-0.5, -0.5);
+  endShape();
+  //console.log(colorPicker.color().levels[3])
+  let c1 = normalizar(colorPicker.color().levels,1);
+  let c2 = normalizar(colorPicker2.color().levels,1);
+  b = slider.value(); 
+  //console.log(c1);
+  colorShader.setUniform('uMaterial1', c1);
+  colorShader.setUniform('uMaterial2', c2);
+  colorShader.setUniform('b', b);
+  
+}
+
+function keyPressed() {
+  if (key == 'c') {
+    //normalizar(colorPicker.color().levels,1)
+    let c1 = normalizar(colorPicker.color().levels,1);
+    let c2 = normalizar(colorPicker2.color().levels,1);
+    c1[3] = slider.value(); 
+    c2[3] = slider.value(); 
+    console.log(c1);
+    colorShader.setUniform('uMaterial1', c1);
+    colorShader.setUniform('uMaterial2', c2);
+  }
+}
+
+function normalizar(arr, max) {
+  // find the max value
+  var m = 0;
+  for(var x=0; x<arr.length; x++) {
+    m = Math.max(m, arr[x]);
+  }
+  // find the ratio
+  var r = max / m;
+  // normalize the array
+  for(var x=0; x<arr.length; x++) arr[x] = arr[x] * r;
+  return arr;
 }
